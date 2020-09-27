877f3a467f1a6bb4508ee193d910c35792e152cd
Viktor geozukunft <geozukunft@gmail.com>
2020-09-28 01:05:17 +0200

    Added possibility to post multiple users. Work by using Twitter Lists. Twitter Lists are from know own needed. This nearly fully dynamic

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ consumer_secret = os.getenv('consumer_secret')
 access_token = os.getenv('access_token')
 access_token_secret = os.getenv('access_token_secret')
 webhook_url = os.getenv('webhook_url')
-twitter_user = os.getenv('twitter_user')
+twitter_list = os.getenv('twitter_list')
 
 
 async def main():
@@ -26,9 +26,9 @@ async def main():
     auth.set_access_token(access_token, access_token_secret)
     api = tw.API(auth, wait_on_rate_limit=True)
 
-    tweets = api.user_timeline(screen_name=twitter_user,
+    tweets = api.list_timeline(list_id=twitter_list,
                                # 200 is the maximum allowed count
-                               count=5,
+                               count=10,
                                include_rts=True,
                                # Necessary to keep full_text
                                # otherwise only the first 140 words are extracted
@@ -42,38 +42,39 @@ async def main():
     tweets_to_send.reverse()
 
     for info in tweets_to_send:
-            row = await conn.fetchrow('SELECT * FROM tweets WHERE tweetid = $1', info.id)
-            if row is None:
-                await conn.execute('INSERT INTO tweets VALUES ($1)', info.id)
-                if info.retweeted_status:
-                    tweettype = "retweeted"
-                else:
-                    tweettype = "tweeted"
+        row = await conn.fetchrow('SELECT * FROM tweets WHERE tweetid = $1', info.id)
+        if row is None:
+            await conn.execute('INSERT INTO tweets VALUES ($1)', info.id)
+            if hasattr(info, 'retweeted_status'):
+                tweettype = "retweeted"
+            else:
+                tweettype = "tweeted"
 
-                data = "@" + info.author.screen_name + " " + tweettype + " this at " + str(info.created_at) + \
-                       ": https://twitter.com/" + info.author.screen_name + "/status/" + info.id_str
-                push_data = {
-                    'content': data,
-                    'username': info.author.name,
-                    'avatar_url': info.author.profile_image_url,
-                }
+            data = "@" + info.author.screen_name + " " + tweettype + " this at " + str(info.created_at) + \
+                   ": https://twitter.com/" + info.author.screen_name + "/status/" + info.id_str
+            push_data = {
+                'content': data,
+                'username': info.author.name,
+                'avatar_url': info.author.profile_image_url,
+            }
 
-                response = requests.post(
-                    webhook_url, data=json.dumps(push_data),
-                    headers={'Content-Type': 'application/json'}
+            response = requests.post(
+                webhook_url, data=json.dumps(push_data),
+                headers={'Content-Type': 'application/json'}
+            )
+            if response.status_code != 200 and response.status_code != 204:
+                raise ValueError(
+                    'Request to discord returned an error %s, the response is:\n%s'
+                    % (response.status_code, response.text)
                 )
-                if response.status_code != 200 and response.status_code != 204:
-                    raise ValueError(
-                        'Request to discord returned an error %s, the response is:\n%s'
-                        % (response.status_code, response.text)
-                    )
 
-    #print(f'done with {tweets_to_send}')
+    # print(f'done with {tweets_to_send}')
     await asyncio.sleep(20)
 
 
 if __name__ == "__main__":
     import time
+
     s = time.perf_counter()
     asyncio.run(main())
     elapsed = time.perf_counter() - s
